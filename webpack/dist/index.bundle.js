bbdd9498d07f536a66bebd6a6710691a09391050
lwrench <1243115181@qq.com>
2023-11-17 22:59:25 +0800

    feat: webpack

---
+++ webpack/dist/index.bundle.js
@@ -0,0 +1,36 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkwebpack"] = self["webpackChunkwebpack"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const test2 = __webpack_require__(/*! ./test2 */ \"./src/test2.js\")\n\nfunction test() {\n  console.log('test')\n}\n\n\ntest()\ntest2()//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxtQkFBTyxDQUFDLCtCQUFTOztBQUUvQjtBQUNBO0FBQ0E7OztBQUdBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLy4vc3JjL2luZGV4LmpzP2I2MzUiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdGVzdDIgPSByZXF1aXJlKCcuL3Rlc3QyJylcblxuZnVuY3Rpb24gdGVzdCgpIHtcbiAgY29uc29sZS5sb2coJ3Rlc3QnKVxufVxuXG5cbnRlc3QoKVxudGVzdDIoKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ }),
+
+/***/ "./src/test2.js":
+/*!**********************!*\
+  !*** ./src/test2.js ***!
+  \**********************/
+/***/ ((module) => {
+
+eval("function test2() {\n  console.log('test2')\n}\n\n\nmodule.exports = test2//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvdGVzdDIuanMiLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBOzs7QUFHQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2svLi9zcmMvdGVzdDIuanM/YjdiMSJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiB0ZXN0MigpIHtcbiAgY29uc29sZS5sb2coJ3Rlc3QyJylcbn1cblxuXG5tb2R1bGUuZXhwb3J0cyA9IHRlc3QyIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/test2.js\n");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+])
