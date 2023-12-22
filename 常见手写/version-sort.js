c77b6c95697d8150698e84639012c3654684d0f9
lwrench <36160633+lwrench@users.noreply.github.com>
2023-12-22 11:57:50 +0800

    Update version-sort.js

--- 常见手写/version-sort.js
+++ 常见手写/version-sort.js
@@ -1,5 +1,23 @@
 const versionList = ['1.1.2', '1.0.2', '1.1.0', '11.1.2']
 
-function versionSort() {
+function versionSort(arr) {
+  return arr.sort(compare)
+}
 
+const delimiter = '.';
+
+function compare(v1, v2) {
+  v1 = String(v1);
+  v2 = String(v2);
+  if(v1 === v2){return 0;}
+  var v1s = v1.split(delimiter);
+  var v2s = v2.split(delimiter);
+  var len = Math.max(v1s.length, v2s.length);
+  for(var i=0; i<len; i++){
+    v1s[i] = "undefined"===typeof v1s[i] ? 0 : parseInt(v1s[i], 10);
+    v2s[i] = "undefined"===typeof v2s[i] ? 0 : parseInt(v2s[i], 10);
+    if(v1s[i] > v2s[i]){return 1;}
+    if(v1s[i] < v2s[i]){return -1;}
+  }
+  return 0;
 }
